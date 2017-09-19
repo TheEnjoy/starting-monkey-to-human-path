e5e011b61cd9e21568362a79d4d49c9c23ddd01b
TheEnjoy <san163ky@mail.ru>
2017-09-19 14:15:16 +0400

    Update Application.java

--- src/PO53/KuznecovAA/wdad/Application.java
+++ src/PO53/KuznecovAA/wdad/Application.java
@@ -4,4 +4,8 @@ package PO53.KuznecovAA.wdad;
  * Created by U_LIVT35407 on 19.09.2017.
  */
 public class Application {
+    public static  void main(String[] args){
+        System.out.println("I'm Kuznecovm and I'm not a monkey");
+    }
+
 }
